ec11a5ba051819df0f7428a10c874ff9b6e72731
ahmedraza67 <ahmedraza@codiea.io>
2026-08-06 13:10:10 +0500

    auth breeze

--- firstproj/routes/web.php
+++ firstproj/routes/web.php
@@ -1,28 +1,20 @@
 <?php
 
-use App\Http\Controllers\FormController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/home', [HomeController::class, 'index']);
-Route::get('/post/{id}', [HomeController::class, 'show']);
-Route::get('/form', [FormController::class, 'index']);
-Route::post('/form/submit', [FormController::class, 'dataPrint']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/product', [ProductController::class, 'create']);
-
-Route::post('/addproduct', [ProductController::class, 'store']);
-
-
-Route::get('/post', [PostController::class, 'create']);
+require __DIR__.'/auth.php';
